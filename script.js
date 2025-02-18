490a812e5e3ee92cc11c8ce43f80c7c669b87c67
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-18 08:31:08 +0200

    Add styling to announcement container

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const searchBar = document.querySelector(".search-bar")
 searchIcon.addEventListener("click", () => {
     searchBar.focus()
 })
+
+// For setting announcement overflow
+document.querySelectorAll(".announcement p").forEach(p => {
+    let maxChars = 90; // Adjust as needed
+    if (p.textContent.length > maxChars) {
+        p.textContent = p.textContent.slice(0, maxChars) + "...";
+    }
+});
